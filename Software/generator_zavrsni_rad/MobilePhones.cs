cfdbb22cc6dd47bf6f7aeebdcdf7574ba3e2c888
Karlo <kkranjcin20@foi.hr>
2023-06-20 14:11:02 +0200

    Dodavanje relativne putanje do projektnog direktorija Dodavanje generirane klase radilo je na principu da se u .csproj datoteku doda stavka "Compile" s apsolutnom putanjom do generiranih klasa što ne bi radilo u slučaju da se promijeni struktura direktorija ili klonira repozitorij na drugo mjesto. Sad je dodana varijabla $(ProjectDir) kojom bi rješenje trebalo raditi bez obzira na putanju do generirane klase.

---
+++ Software/generator_zavrsni_rad/MobilePhones.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace generator_zavrsni_rad
+{
+    class MobilePhones
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int RAM { get; set; }
+
+        public DateTime LaunchDate { get; set; }
+
+        public string OS { get; set; }
+    }
+}
