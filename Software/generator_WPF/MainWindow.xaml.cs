aa082c189be5fcde9fffec6bfd0e6c1d1b0dd42a
Karlo <kkranjcin20@foi.hr>
2023-07-01 11:57:29 +0200

    Dodavanje potrebne using direktive za Regex

--- Software/generator_WPF/MainWindow.xaml.cs
+++ Software/generator_WPF/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Windows;
 
 namespace generator_WPF
