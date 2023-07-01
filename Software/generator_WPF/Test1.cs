7328a4ea7de378debb84a5ae926c9f8544de4cbc
Karlo <kkranjcin20@foi.hr>
2023-07-01 13:22:04 +0200

    Izbrisana slika ikone zbog problema s pokretanjem ekstenzije

--- Software/generator_WPF/Test1.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace generator_WPF
-{
-    class Test1
-    {
-        public int Id { get; set; }
-    }
-}
--- Software/generator_WPF/Test2.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace generator_WPF
-{
-    class Test2
-    {
-        public string Name { get; set; }
-    }
-}
